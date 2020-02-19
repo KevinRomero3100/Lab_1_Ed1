b5b39bbab091db8f5d24e65d8b23a19988a4c742
KEVIN HUMBERTO ROMERO VILLALTA <kevinromero3100@icloud.com>
2020-02-19 15:24:46 -0600

    First comit

---
+++ GenericLibrary/Interfaces/StructureBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos.Interfeces
+{
+    public abstract class StructutreBase<T>
+
+    {
+        protected abstract void Insert(T value);
+        protected abstract void Delete(T value);
+        protected abstract T Get(T value);
+    }
+}
---
+++ GenericLibrary/Structures/ListProp.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Genericos.Interfeces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Genericos.Structures
+{
+    public class ListProp<T>: StructutreBase<T>, IEnumerable<T>
+    {
+        public Node<T> Head { get; set; }
+
+        public void Add (T value)
+        {
+            Insert(value);
+        }
+		public void Del(T value)
+		{
+			Delete(value);
+		}
+		public void Extract(T value)
+		{
+			Get(value);
+		}
+
+        protected override void Insert(T value)
+        {
+
+			Node<T> NewNode = new Node<T>();
+			NewNode.Value = value;
+
+			if (Head == null)
+			{
+				Head = NewNode;
+			}
+			else
+			{
+				Node<T> search;
+
+				search = Head;
+				while (search.Next != null)
+				{
+					search = search.Next;
+				}
+				search.Next = NewNode;
+			}
+		}
+
+		protected override void Delete(T value)
+		{
+			Node<T> search;
+			Node<T> last = null;
+			search = Head;
+
+			if (!Head.Value.Equals(value))
+			{
+				Head = Head.Next;
+			}
+			else
+			{
+				while (!search.Value.Equals(value))
+				{
+					last = search;
+					search = search.Next;
+				}
+				if (search.Next == null)
+				{
+					last.Next = null;
+				}
+				else
+				{
+					last.Next = search.Next;
+				}
+
+			}
+		}
+
+		protected override T Get(T value)
+		{
+			Node<T> search;
+			search = Head;
+			if (Head == null)
+			{
+				return Head.Value;
+			}
+			else
+			{
+				if (value.Equals(Head.Value))
+				{
+					return Head.Value;
+				}
+				else
+				{
+					while (!search.Value.Equals(value) && search.Next == null)
+					{
+						search = search.Next;
+
+					}
+					if (search.Next.Equals(value))
+					{
+						return search.Value;
+					}
+					else
+					{
+						value = default(T);
+						return value;
+					}
+
+				}
+			}
+			
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
